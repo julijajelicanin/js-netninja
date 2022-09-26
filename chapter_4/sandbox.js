2cb5fbe9fa479fa604bd751aec5c1845689c75a6
Julija Jelicanin <julija@Jovan-2.local>
2022-09-26 11:49:07 +0200

    modified js functions

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,7 +1,7 @@
 function greet(){
     console.log("hello");
 }
-;
+
 const speak = function(){
     console.log("good day");
 };
@@ -9,3 +9,67 @@ const speak = function(){
 greet();
 
 speak();
+
+//arguments and parametres
+const greeting = function(name){
+    console.log(`good day ${name}`);
+};
+
+const greetings_2 =function(name, surname){
+    console.log(`Hi ${name} ${surname}`);
+};
+
+greeting('julija');
+greetings_2('Julija','Jelicanin');
+
+//returning values-regulR FUNCTION
+ const calcArea = function(radius){
+    return 3.14*radius**2;
+ };
+
+ const area= calcArea(5);
+ console.log(area);
+
+ //arrow function
+ const calculateArea =(radius)=>{
+    return 3.14*radius**2;
+ };
+
+ const area_2 =calculateArea(5);
+ console.log('area is:' ,area_2);
+
+ //practice functions
+ const practice= function(){
+    return 'hello world';
+ };
+
+ const practice1=()=>{
+    return 'hello world';
+ }
+
+ //practice 2
+
+ const bill = function(products, tax){
+
+    let total=0;
+    for(let i=0; i< products.length; i++)
+    {
+        total+=products[i] +products[i]*tax;
+    }
+    return total;
+ }
+
+
+ function bill1(products, tax) {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+        total += products[i] + products[i] * tax;
+    }
+    return total;
+}
+
+ //methods
+ const name2 = "shaun";
+
+ let resultTwo = name2.toUpperCase();
+ console.log(resultTwo);
